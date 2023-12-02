a95b3977ec04f17f384c334f59251dea34ec3d94
Mahyar Majidi <mahyar.majidi@outlook.com>
2023-12-02 14:13:37 +0330

    Registring Model

--- storefront/urls.py
+++ storefront/urls.py
@@ -18,6 +18,10 @@ from django import urls
 from django.contrib import admin
 from django.urls import path, include
 
+# to change admin panel title
+admin.site.site_header = "Storefront Admin"
+admin.site.index_title = 'Admin'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('playground/', include('playground.urls')),
