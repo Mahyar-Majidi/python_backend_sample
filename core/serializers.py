5b8e14a025ca79ab3372637dc65bc2de21e53ec3
Mahyar Majidi <mahyar.majidi@outlook.com>
2023-12-18 11:21:57 +0330

    Signals

--- core/serializers.py
+++ core/serializers.py
@@ -1,8 +1,8 @@
-from djoser.serializers import UserCreateSerializer as BaseUserSerializer, UserSerializer as BaseUserSerializer
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer, UserSerializer as BaseUserSerializer
 
 
-class UserCreateSerializer(BaseUserSerializer):
-    class Meta(BaseUserSerializer.Meta):
+class UserCreateSerializer(BaseUserCreateSerializer):
+    class Meta(BaseUserCreateSerializer.Meta):
         fields = ['id', 'username', 'password',
                   'email', 'first_name', 'last_name']
 
