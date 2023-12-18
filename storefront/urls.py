1c5dc31516533508215c6b94c94e19388ba199a3
Mahyar Majidi <mahyar.majidi@outlook.com>
2023-12-18 13:18:09 +0330

    Managing Media Files

--- storefront/urls.py
+++ storefront/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django import urls
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 # to change admin panel title
 admin.site.site_header = "Storefront Admin"
@@ -30,3 +32,6 @@ urlpatterns = [
     path('auth/', include('djoser.urls.jwt')),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
